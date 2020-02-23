fa249b78d451bf50e419f928065920eb265a6c50
Koulick Sankar Paul <koulick89@gmail.com>
2020-02-23 09:58:28 -0600

    second solution

--- ArraysAndStrings/src/main.java
+++ ArraysAndStrings/src/main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -40,9 +42,107 @@ public class main {
         return true;
     }
 
+    /**
+     * Checks to find if first string is a permutation of the second string
+     * Uses sorting (Dual-Pivot quick sort)
+     * Runtime: O(nlogn)
+     * @param firstInput the first input string
+     * @param secondInput the second input string
+     * @return true if first string is a permutation of the second string, false otherwise
+     */
+    private static boolean isPermutationOf(String firstInput, String secondInput) {
+        if(firstInput.length() != secondInput.length()) {
+            return false;
+        }
+        char[] firstInputCharArray = firstInput.toCharArray();
+        char[] secondInputCharArray = secondInput.toCharArray();
+        Arrays.sort(firstInputCharArray);
+        Arrays.sort(secondInputCharArray);
+
+        return Arrays.equals(firstInputCharArray, secondInputCharArray);
+    }
+
+    /**
+     * Checks to find if first string is a permutation of the second string,
+     * using ASCII values
+     * Runtime: O(n)
+     * @param firstInput the first input string
+     * @param secondInput the second input string
+     * @return true if first string is a permutation of the second string, false otherwise
+     */
+    private static boolean isPermutationOfUsingAsciiValues(String firstInput, String secondInput) {
+        if(firstInput.length() != secondInput.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[256];
+
+        for (int i=0;  i<firstInput.length(); i++) {
+            charCount[firstInput.charAt(i)]++;
+            charCount[secondInput.charAt(i)]--;
+        }
+
+        for (int i=0; i<charCount.length; i++){
+            if(charCount[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks to find if first string is a permutation of the second string,
+     * using hashmaps
+     * Runtime: O(n)
+     * @param firstInput the first input string
+     * @param secondInput the second input string
+     * @return true if first string is a permutation of the second string, false otherwise
+     */
+    private static boolean isPermutationOfUsingHashmap(String firstInput, String secondInput) {
+        if(firstInput.length() != secondInput.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> characterIntegerHashMap = new HashMap<>();
+        for (char charFromfirstInput : firstInput.toCharArray()) {
+            if (!characterIntegerHashMap.containsKey(charFromfirstInput)) {
+                characterIntegerHashMap.put(charFromfirstInput, 1);
+            } else {
+                characterIntegerHashMap.put(charFromfirstInput, characterIntegerHashMap.get(charFromfirstInput)+1);
+            }
+        }
+
+        for(char charFromSecondInput : secondInput.toCharArray()) {
+            if(characterIntegerHashMap.containsKey(charFromSecondInput)) {
+                characterIntegerHashMap.put(charFromSecondInput, characterIntegerHashMap.get(charFromSecondInput)-1);
+            } else {
+                // does not contain the key, i.e. the current character in the second input string,
+                // which means that the first input and the second input have different characters
+                // Hence, input strings do not match.
+                return false;
+            }
+        }
+
+        for(Character character : characterIntegerHashMap.keySet()) {
+            if (characterIntegerHashMap.get(character) != 0) {
+                // the map has this 'char' whose value is not 0
+                // Hence, input strings do not match.
+                return false;
+            }
+            return true;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        String input = "test";
+        String input = "tesla";
         System.out.println("Is input unique? - " + isUniqueUsingInt(input));
         System.out.println("Is input unique? - " + isUniqueUsingSet(input));
+
+        String firstInput = "aba";
+        String secondInput = "aab";
+        System.out.println(firstInput + " is a permutation of " + secondInput + ": " + isPermutationOf(firstInput, secondInput));
+        System.out.println(firstInput + " is a permutation of " + secondInput + ": " + isPermutationOfUsingAsciiValues(firstInput, secondInput));
+        System.out.println(firstInput + " is a permutation of " + secondInput + ": " + isPermutationOfUsingHashmap(firstInput, secondInput));
     }
 }
